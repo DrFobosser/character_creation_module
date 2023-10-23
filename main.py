c86875194481db27bab61430d9362d82f5c389a3
fobos <fobossser@yandex.ru>
2023-10-23 15:04:05 +0300

    without classes

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ def attack(char_name, char_class):
     if char_class == 'mage':
         return f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}'
     if char_class == 'healer':
-        return f'{char_name} нанёс урон противнику равный {5+randint(-3, -1)}'
+        return f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}'
     return ()
 
 
 def defence(char_name, char_class):
     if char_class == 'warrior':
-        return f'{char_name} блокировал {10+randint(5, 10)} урона'
+        return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
-        return f'{char_name} блокировал {10+randint(-2, 2)} урона'
+        return f'{char_name} блокировал {10 + randint(-2, 2)} урона'
     if char_class == 'healer':
-        return f'{char_name} блокировал {10+randint(2, 5)} урона'
+        return f'{char_name} блокировал {10 + randint(2, 5)} урона'
     return ()
 
 
